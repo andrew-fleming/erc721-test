37ec8c02010f5019050f4a2de3896a40f570c10e
andrew <fleming-andrew@protonmail.com>
2020-11-14 01:19:11 -0500

    added color graphics, need to fix mint function in js

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,61 +1,155 @@
 import React, {useState, useEffect} from 'react'
 import Web3 from 'web3'
+import styled from 'styled-components'
 import Color from '../abi/Color'
 import NavBar from './NavBar' 
-import Colors from './Colors'
+
+const Row = styled.div`
+    background-color: blue;
+    width: 100%;
+    height: 20rem;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+`;
+
+const Align = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    `;
+
+const Content = styled.div`
+    margin-top: 2rem;
+   `;
+
+const Circle = styled.div`
+        height: 150px;
+        width: 150px;
+        border-radius: 50%;
+        display: inline-block;
+`;
+
+//forms
+
+const FormDiv = styled.div`
+    display: flex;
+    justify-content: space-around;
+`;
+
+const Form = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const Input = styled.input`
+    height: 2.7rem;
+`;
+
+const Button = styled.button`
+    width: 10.3rem;
+    height: 3rem;
+    margin-top: .3rem;
+`;
+
+
+const web3 = new Web3(Web3.givenProvider)
+const contractAddress = '0x3d6091236dce96BBFDFFF8557CeEaD29D517357C'
+const contract = new web3.eth.Contract(Color.abi, contractAddress)
 
 export default function Main() {
 
-    let contract
-
-    const [usingWeb3, setUsingWeb3] = useState(false)
     const [account, setAccount] = useState('')
     const [totalSupply, setTotalSupply] = useState(0)
     const [colors, setColors] = useState([])
-
-
-    const loadWeb3 = async function() {
-        if(window.ethereum) {
-            window.web3 = new Web3(window.ethereum)
-            await window.ethereum.enable()
-        }
-        else if (window.web3) {
-            window.web3 = new Web3(window.web3.currentProvider)
-        }
-        else {
-            window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
-        }
-    }
-
-    const loadBlockchainData = async() => {
-        const web3 = window.web3
-        const accounts = await web3.eth.getAccounts()
-        setAccount(accounts)
-
-        const networkId = await web3.eth.net.getId()
-        const networkData = Color.networks[networkId]
-        if(networkData) {
-            const address = networkData.address
-            const abi = Color.abi
-            contract = new web3.eth.Contract(abi, address)
-        } else {
-            alert('Smart contract not deployed to network')
-        }
-    }
+    const [addColor, setAddColor] = useState(true)
+    const [newColor, setNewColor] = useState('')
 
     useEffect(() => {
-        if(!usingWeb3){
-            loadWeb3()
-            .then(loadBlockchainData())
+        const loadAccount = async() => {
+            setAccount(await web3.eth.getAccounts())
         }
-        setUsingWeb3(true)
-    }, [usingWeb3])
+        loadAccount()
+    }, [account])
+
+
+    useEffect(() => {
+        const loadSupply = async() => {
+            setTotalSupply(await contract.methods.totalSupply().call())
+            console.log(totalSupply)
+        }
+        loadSupply()
+    }, [totalSupply])
+    
+    
+    useEffect(() => {
+        const loadColors = async() => {
+            for(var i = 1; i <= totalSupply; i++){
+                const color = await contract.methods.colors(i - 1).call()
+                if(addColor){
+                    setColors(colors => [...colors, color])
+                    setAddColor(false)
+                }
+            }
+        }
+            loadColors()
+    }, [totalSupply, addColor])
+
+
+    //mint function
+
+    const handleInput = (event) => {
+        setNewColor(event.target.value)
+    }
+
+
+    const mintToken = async() => {
+        let config = {
+            from: account[0]
+        }
+        contract.methods.mint(newColor).send(config)
+            .on(receipt => {
+                console.log(receipt)
+                setAddColor(true)
+        })
+    }
+    
+    
 
     return (
         <div>
-            <NavBar account={account}>
+            <NavBar account={account}/>
 
-            </NavBar>
+            <Row>
+                {colors.map((color, key) => {
+                    return(
+                        <Align key={key}>
+                        <Circle style={{ backgroundColor: color }} ></Circle>
+                        <Content>{color}</Content>
+                        </Align>
+                        )
+                })} 
+            </Row>
+
+            <FormDiv>
+                <Form>
+                    <h2>Issue Token</h2>
+                    <Input 
+                        placeholder="E.g. #FFFFFF"
+                        onChange={handleInput}
+                        />
+                    <div>
+                        <Button 
+                        onClick={mintToken}
+                        >
+                            Mint
+                        </Button>
+                    </div>
+                </Form>
+            </FormDiv>
         </div>
+        
     )
 }
